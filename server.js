0d13253a0ea7f32eaaa2f55a81cce61232f03e08
Dharmik79 <dholariyadharmik7@gmail.com>
2021-04-21 16:53:56 +0530

    home page added

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express=require('express');
 const app=express();
-const PORT=process.env.PORT || 3000;
-const ejs=require('ejs')
-const expressLayout=require('express-ejs-layouts')
-const path=require('path')
+const PORT=3000;
+const ejs=require('ejs');
+const expressLayout=require('express-ejs-layouts');
+const path=require('path');
+
+
+app.get('/',async(req,res)=>{
+    res.render('home')
+});
 
 // set Template engine
 app.use(expressLayout); 
+
 app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
+app.set('view engine','ejs');
+
 
 // Assests
 app.use(express.static('public'))
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js/appp.js').sass('resources/scss/app.scss','public/scss/app.css');
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss','public/css/app.css');
