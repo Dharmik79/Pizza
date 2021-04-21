e7ebb9fb0a1285f53c4bf3dd54cf0a7545f38551
Dharmik79 <dholariyadharmik7@gmail.com>
2021-04-22 00:35:14 +0530

    cart login

--- server.js
+++ server.js
@@ -5,18 +5,26 @@ const ejs=require('ejs');
 const expressLayout=require('express-ejs-layouts');
 const path=require('path');
 
-
-app.get('/',async(req,res)=>{
-    res.render('home')
-});
-
 // set Template engine
 app.use(expressLayout); 
 
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs');
 
+app.get('/',async(req,res)=>{
+    res.render('home')
+});
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
 
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 // Assests
 app.use(express.static('public'))
 app.listen(PORT,()=>{
